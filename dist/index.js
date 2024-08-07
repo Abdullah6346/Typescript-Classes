4ef2941a1b2c206a47d8a3f038f41e757a30b8b1
Muhammad Abdullah <139040726+Abdullah6346@users.noreply.github.com.>
2024-08-07 12:44:40 +0500

    feat: Modern Way of writing Constructors

--- dist/index.js
+++ dist/index.js
@@ -8,5 +8,24 @@ class User {
         this.city = city;
     }
 }
+class Readonly {
+    constructor(username) {
+        this.username = username;
+    }
+}
 const userone = new User("gladdy", "gladdy@.com", "Faisalabad");
+// userone.city not possible
+const user = new Readonly("gladdy");
+// user.username Possible in Readonly
 console.log(userone);
+// Modern Way: to write classes constructures
+class Students {
+    constructor(name, email, id) {
+        this.name = name;
+        this.email = email;
+        this.id = id;
+    }
+}
+const firstStudent = new Students("gladdy", "gladdy1234@gmail.com", "8-990-9");
+const secondStudent = new Students("Umar", "Umar664@gmail.com", "890-98-98");
+console.log(firstStudent, secondStudent);
--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,27 @@ class User{
         this.city=city
     }
 } 
+class Readonly{
+   readonly username:string
+   constructor(username:string){
+    this.username=username
+   }
+}
+
+
 const userone = new User("gladdy","gladdy@.com","Faisalabad")
+// userone.city not possible
+const user=new Readonly("gladdy")
+// user.username Possible in Readonly
 console.log(userone)
+
+// Modern Way: to write classes constructures
+class Students{
+    constructor ( public name:string,public email:string,private id:string){
+
+    }
+}
+const firstStudent=new Students("gladdy","gladdy1234@gmail.com","8-990-9")
+const secondStudent=new Students("Umar","Umar664@gmail.com","890-98-98")
+console.log(firstStudent,secondStudent)
 export{}
