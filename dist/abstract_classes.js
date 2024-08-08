cf52e54228dae69ae542d2aaa5b363a4113d782f
Muhammad Abdullah <139040726+Abdullah6346@users.noreply.github.com.>
2024-08-08 14:56:19 +0500

    feat:(Abstact Classes Completed)

--- dist/abstract_classes.js
+++ dist/abstract_classes.js
@@ -8,8 +8,19 @@ class TakePhoto {
     }
 }
 class Instagram extends TakePhoto {
+    constructor(cameraMode, darkMode, filter) {
+        super(cameraMode, darkMode, filter);
+        this.cameraMode = cameraMode;
+        this.darkMode = darkMode;
+        this.filter = filter;
+    }
     getsapia() {
         return `Hello I am not defined in the abstarct class`;
     }
+    getcameramode() {
+        return `Camera Mode is ${this.cameraMode}`;
+    }
 }
 const firstperson = new Instagram('black', true, 'filtered');
+console.log(firstperson.getcameramode());
+console.log(firstperson.getsapia());
--- src/abstract_classes.ts
+++ src/abstract_classes.ts
@@ -7,10 +7,23 @@ abstract class TakePhoto {
   abstract getsapia(): string;
 }
 class Instagram extends TakePhoto {
-  getsapia() {
+  constructor(
+    public cameraMode: string,
+    public darkMode: boolean,
+    public filter: string,
+  ) {
+    super(cameraMode, darkMode, filter);
+  }
+  getsapia(): string {
     return `Hello I am not defined in the abstarct class`;
+  }
+  getcameramode(): string {
+    return `Camera Mode is ${this.cameraMode}`;
   }
 }
 
 const firstperson = new Instagram('black', true, 'filtered');
+
+console.log(firstperson.getcameramode());
+console.log(firstperson.getsapia());
 export {};
