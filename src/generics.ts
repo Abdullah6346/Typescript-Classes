203d9771bc67f08d148fc983b759a94e4fe524ec
Muhammad Abdullah <139040726+Abdullah6346@users.noreply.github.com.>
2024-08-09 13:00:14 +0500

    feat: Generics as an interface

--- src/generics.ts
+++ src/generics.ts
@@ -30,8 +30,20 @@ function get_products<t>(array: t[]): t {
   return array[index];
 }
 console.log(get_products([0, 1, 2, 3]));
-
+// TODO: WE USE HERE , FOR BEING NOT CONFUSED <T ,>
 const getmore_produc = <T>(array: T[]): T[] => {
   return array;
 };
 console.log(getmore_produc(['Sugar', 'Cup', 'Milk', 'tea']));
+
+// TODO: mORE THAN ONE GENERICS
+
+interface id {
+  userid: string;
+  name: string;
+}
+
+function getuserd<Type extends id>(user: Type): string {
+  return `User id is ${user.userid}`;
+}
+console.log(getuserd({ userid: '665544', name: 'Gladdy' }));
